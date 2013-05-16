423213e7e7a6ba252cb3e39c1c1d9fb9dee163cc
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-05-16 08:52:00 +0100

    Add the test project

---
+++ Parsing.Linq.Test/ParserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parsing.Linq.Test
+{
+    [TestClass]
+    public class ParserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
