326b301c8d1f257e0bbf988eddf4e25d63985331
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-05-16 11:24:16 +0100

    Add comments to the Cast method.

--- Parsing.Linq/Parser.cs
+++ Parsing.Linq/Parser.cs
@@ -9,6 +9,16 @@
             return ParserExtensions.Create(input => p1.Parse(input) ?? p2.Parse(input));
         }
 
+        /// <remarks>
+        /// This method was added directly to the Parse class to get better syntax.
+        /// It is conversion from one type to another. In order to pass the information
+        /// about types one could you create an extension method but the call would
+        /// like Cast[T1, T2]. When this is an istance method one type is taken from
+        /// the instance and only the second is passed explicitly. Such code is similar
+        /// to Enumerable.Cast.
+        /// </remarks>
+        /// <typeparam name="T2"></typeparam>
+        /// <returns></returns>
         public Parser<T2> Cast<T2>()
         {
             return this.Select(_ => (T2)(object)_);
