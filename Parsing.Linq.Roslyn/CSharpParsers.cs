791bca8f0b948ac34a1df49238744b99da4ddb17
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-05-18 15:39:50 +0100

    Create Parsing.Linq.Roslyn project which wraps Roslyn parsers into monads

---
+++ Parsing.Linq.Roslyn/CSharpParsers.cs
@@ -0,0 +1,33 @@
+﻿using Roslyn.Compilers.CSharp;
+using Roslyn.Compilers.Common;
+
+namespace System.Parsing.Linq
+{
+    public static class CSharpParsers
+    {
+        public static Parser<T> FromSyntaxParse<T>(Func<string, int, T> func) where T : CommonSyntaxNode
+        {
+            return ParserExtensions.Create(input =>
+                {
+                    var syntax = func(input, 0);
+                    return syntax.IsMissing ? null :
+                        new ParserResult<T>(syntax, input.Substring(syntax.FullSpan.Length));
+                });
+        }
+
+        public static Parser<T> FromSyntaxParse<T>(Func<string, int, ParseOptions, T> func, ParseOptions options = null) where T : CommonSyntaxNode
+        {
+            return ParserExtensions.Create(input =>
+                {
+                    var syntax = func(input, 0, options);
+                    return syntax.IsMissing ? null :
+                        new ParserResult<T>(syntax, input.Substring(syntax.FullSpan.Length));
+                });
+        }
+
+        public static readonly Parser<NameSyntax> Identifier = FromSyntaxParse(Syntax.ParseName);
+        public static readonly Parser<ExpressionSyntax> Expression = FromSyntaxParse(Syntax.ParseExpression);
+        public static readonly Parser<TypeSyntax> TypeName = FromSyntaxParse(Syntax.ParseTypeName);
+        public static readonly Parser<ParameterListSyntax> ParameterList = FromSyntaxParse(Syntax.ParseParameterList);
+    }
+}
