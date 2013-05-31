6891a8ad896a60d68c567472480ce3ed56f24059
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-05-31 20:04:46 +0100

    Add empty test ptoject Parsing.Linq.Roslyn.Test

---
+++ Parsing.Linq.Roslyn.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Parsing.Linq;
+using System.Resources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parsing.Linq.Roslyn.Test
+{
+    [TestClass]
+    public class CSharpErrorParsersTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
