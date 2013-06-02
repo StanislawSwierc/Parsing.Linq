fa136da6ab3cae039bad64085b6997f6dbfa39a7
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-06-02 10:10:38 +0100

    Remove the "Is" prefix from all CharParsers

--- Parsing.Linq/CharParsers.cs
+++ Parsing.Linq/CharParsers.cs
@@ -2,23 +2,23 @@
 {
     public static class CharParsers
     {
-        public static readonly Parser<char> IsControl = Parser.FromChar(char.IsControl);
-        public static readonly Parser<char> IsDigit = Parser.FromChar(char.IsDigit);
-        public static readonly Parser<char> IsHighSurrogate = Parser.FromChar(char.IsHighSurrogate);
-        public static readonly Parser<char> IsLetter = Parser.FromChar(char.IsLetter);
-        public static readonly Parser<char> IsLetterOrDigit = Parser.FromChar(char.IsLetterOrDigit);
-        public static readonly Parser<char> IsLowSurrogate = Parser.FromChar(char.IsLowSurrogate);
-        public static readonly Parser<char> IsLower = Parser.FromChar(char.IsLower);
-        public static readonly Parser<char> IsNumber = Parser.FromChar(char.IsNumber);
-        public static readonly Parser<char> IsPunctuation = Parser.FromChar(char.IsPunctuation);
-        public static readonly Parser<char> IsSeparator = Parser.FromChar(char.IsSeparator);
-        public static readonly Parser<char> IsSurrogate = Parser.FromChar(char.IsSurrogate);
-        public static readonly Parser<char> IsSymbol = Parser.FromChar(char.IsSymbol);
-        public static readonly Parser<char> IsUpper = Parser.FromChar(char.IsUpper);
-        public static readonly Parser<char> IsWhiteSpace = Parser.FromChar(char.IsWhiteSpace);
+        public static readonly Parser<char> Control = Parser.FromChar(char.IsControl);
+        public static readonly Parser<char> Digit = Parser.FromChar(char.IsDigit);
+        public static readonly Parser<char> HighSurrogate = Parser.FromChar(char.IsHighSurrogate);
+        public static readonly Parser<char> Letter = Parser.FromChar(char.IsLetter);
+        public static readonly Parser<char> LetterOrDigit = Parser.FromChar(char.IsLetterOrDigit);
+        public static readonly Parser<char> LowSurrogate = Parser.FromChar(char.IsLowSurrogate);
+        public static readonly Parser<char> Lower = Parser.FromChar(char.IsLower);
+        public static readonly Parser<char> Number = Parser.FromChar(char.IsNumber);
+        public static readonly Parser<char> Punctuation = Parser.FromChar(char.IsPunctuation);
+        public static readonly Parser<char> Separator = Parser.FromChar(char.IsSeparator);
+        public static readonly Parser<char> Surrogate = Parser.FromChar(char.IsSurrogate);
+        public static readonly Parser<char> Symbol = Parser.FromChar(char.IsSymbol);
+        public static readonly Parser<char> Upper = Parser.FromChar(char.IsUpper);
+        public static readonly Parser<char> WhiteSpace = Parser.FromChar(char.IsWhiteSpace);
 
-        public static readonly Parser<char> IsLeftParen = Parser.FromChar('(');
-        public static readonly Parser<char> IsRightParen = Parser.FromChar(')');
+        public static readonly Parser<char> LeftParen = Parser.FromChar('(');
+        public static readonly Parser<char> RightParen = Parser.FromChar(')');
 
     }
 }
