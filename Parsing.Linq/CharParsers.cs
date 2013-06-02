6482c7b371d61af6dd37ea19a6f065cf0f795b25
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2013-06-02 10:02:31 +0100

    Remove unnecessary using stetements

--- Parsing.Linq/CharParsers.cs
+++ Parsing.Linq/CharParsers.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace System.Parsing.Linq
+﻿namespace System.Parsing.Linq
 {
     public static class CharParsers
     {
